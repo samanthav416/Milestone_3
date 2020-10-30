83b9fa485463ce565637947fb7ea8941b0aa69cf
samanthav416 <70677772+samanthav416@users.noreply.github.com>
2020-10-30 06:57:48 -0400

    Update quiz_js.js

--- quiz_js.js
+++ quiz_js.js
@@ -1,13 +1,15 @@
-const initalValues {
-	name: "";
-	numCorrect:0;
-	totalQuestions: 20;
-	questionNum: 0;	
+const appState = {
+	current_view: 'home_page',
+	name: '',
+	correct_answers: 0,
+	total_questions: 20,
+	question_num : 0,
+	score: 0
 }
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	create_quiz();
+
 	
 	document.querySelector("#quiz_widget").onclick = (e) => {
 		select_quiz(e)
@@ -15,29 +17,31 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const select_quiz = (e) => {
+	
 	if (e.target.dataset.selection == "quiz1") {
-		
+		update_view("#quiz_view")
 	}
 	else if (e.target.dataset.selection == "quiz2") {
-		
-	}
-	
-	
+		update_view("#quiz_view")
+	}	
 }
 
-async function create_quiz(num)
-{
+const create_quiz_view = async (question_num) => {
+
 	const response = await fetch('https://my-json-server.typicode.com/samanthav416/Milestone_3/', { mode: 'no-cors' });
 	const data = await response.json();
-	return data[num]; 
+	return data[question_num]; 
 }
 
+function update_view(current_view){
+	const html_element= render_view({}, current_view)
+	document.querySelector("#quiz_widget").innerHTML = html_element;
+}
 
 const render_view =(model, view) => {
-	template_source = document.querySelector(view).innerHTML;
 	
+	template_source = document.querySelector(view).innerHTML;
 	var template = Handlebars.compile(template_source);
 	var html_widget_element = template(model);
 	return html_widget_element;
 }
-	
